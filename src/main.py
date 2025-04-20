7d35745331a7814700c79bb6b4416041c12d9825
thejimbo27 <thejimbo27@fishhousemafia.com>
2025-04-20 02:36:07 -0500

    sqlite

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import discord
 import os
 from random import Random
 from dotenv import load_dotenv
+import sqlite3
 
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
@@ -10,16 +11,42 @@ token = os.getenv('DISCORD_TOKEN')
 random = Random()
 
 
+sql_connection = sqlite3.connect("players.db")
+cursor = sql_connection.cursor()
+
+def player_table_exists():
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='players'")
+    return cursor.fetchall()
+
+def create_tables():
+    cursor.execute("CREATE TABLE players(name)")
+    return cursor.fetchall()
+
+def player_exists_in_db(player):
+    cursor.execute("SELECT * FROM players WHERE name = ?", (player,))
+    return cursor.fetchall()
+
+def create_player_in_db(player):
+    cursor.execute("INSERT INTO players VALUES(?)", (player,))
+    return cursor.fetchall()
+
 
 class MyClient(discord.Client):
     async def on_ready(self):
         print(f"Logged on as {self.user}!")
+        if not player_table_exists():
+            create_tables()
 
     async def on_message(self, message):
         print(f'Message from {message.author}: {message.content}')
         if message.author == self.user:
             return
         channel = self.get_channel(1363384148227784755)
+        if player_exists_in_db(message.author.id):
+            await channel.send("I've seen you before.")
+        else:
+            await channel.send("You're new.")
+            create_player_in_db(message.author.id)
         msg = random.randint(1,6)
         if msg == 1:
             await channel.send('you look cute v.v')
