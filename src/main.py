6025d398a092dcfe290bf35706303d4a11026e8d
cameron <cmrnwlsh@gmail.com>
2025-04-20 01:39:55 -0400

    ignore pyvenv.cfg

--- src/main.py
+++ src/main.py
@@ -5,12 +5,14 @@ from dotenv import load_dotenv
 load_dotenv()
 token = os.getenv("DISCORD_TOKEN")
 
+
 class MyClient(discord.Client):
     async def on_ready(self):
-        print(f'Logged on as {self.user}!')
+        print(f"Logged on as {self.user}!")
 
     async def on_message(self, message):
-        print(f'Message from {message.author}: {message.content}')
+        print(f"Message from {message.author}: {message.content}")
+
 
 intents = discord.Intents.default()
 intents.message_content = True
