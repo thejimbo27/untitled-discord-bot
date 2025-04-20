dcc47bd510dc1acdbda01a75f06c489054201497
thejimbo27 <thejimbo27@fishhousemafia.com>
2025-04-20 00:23:08 -0500

    use DISCORD_TOKEN variable, defined in /.env

--- src/main.py
+++ src/main.py
@@ -1,4 +1,9 @@
 import discord
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+token = os.getenv("DISCORD_TOKEN")
 
 class MyClient(discord.Client):
     async def on_ready(self):
@@ -11,4 +16,4 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 client = MyClient(intents=intents)
-client.run('my token goes here')
+client.run(token)
